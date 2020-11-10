c4a57b5629cc9c703ad28fff6bfeffb34b22d4d9
breakthatbass <gamache.taylor@gmail.com>
2020-11-10 10:54:08 -0500

    add question and answers

--- process-api/1.c
+++ process-api/1.c
@@ -1,3 +1,22 @@
+/**
+ * chapoter 5: process API
+ * exercise 1: write a program that calls fork(). before calling fork(), have
+ * the main process access a variable (e.g., x) and set its value to something
+ * (e.g., 100). 
+ *
+ * what value is the variable in the child process? 
+ * - the value in the child process is the same as the main process unless
+ *   redefined in the child process. 
+ *
+ *
+ * what happens to the variable when both the parent and the child change the
+ * value of x? 
+ * - the changes are only reflected within each of the processes and have no
+ *   effect on the other processes.
+ *
+ * */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -10,8 +29,9 @@ int main()
         fprintf(stderr, "fork failed...\n");
         exit(1);
     } else if (rc == 0) {   // child process
+		printf("child x (unchanged from parent): %d\n", x);
         x = 101;
-        printf("child x: %d\n", x);
+        printf("child x (changed to 101): %d\n", x);
     } else {    // parent process
         int rc_wait = wait(NULL);   // wait until child process is done
         printf("parent x: %d\n", x);
