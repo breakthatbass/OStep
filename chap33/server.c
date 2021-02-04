69c905d70f5d906d2290763b1a602000849a1347
breakthatbass <gamache.taylor@gmail.com>
2021-02-04 15:31:22 -0500

    redo and simplify the tcp server for question 1

---
+++ chap33/server.c
@@ -0,0 +1,99 @@
+/***
+ * ultra simple TCP web server with error checking
+ *	
+ *	when the server starts up it saves the current time
+ *	and date in a variable. at a successful connection to
+ *	a client, it sends the data through.
+ *
+ *	you can connect to this server through something like
+ *	telnet. if you don't have it use brew install telnet
+ *
+ *	$ telnet hostname PORT
+ *	should connect and you'll see the data
+ *
+ *	or you can wait until i write a client and push it
+ *
+ * */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <netdb.h>
+#include <unistd.h>
+
+#define PORT 3490
+#define BACKLOG 10
+#define MAXDATA 100
+#define SA struct sockaddr
+
+int main()
+{
+	int sockfd, newfd;
+	//struct addrinfo hints, *servinfo, *p;
+	struct sockaddr_in servaddr;
+	struct sockaddr_storage client_addr;
+	socklen_t len;
+	int rv;
+
+	// current time and date
+	// set it in a var to send through the socket
+	time_t t;
+	time(&t);
+	char time[MAXDATA];
+	strcpy(time, "🚀 ");
+	strcat(time, ctime(&t));
+
+	// clear servaddr struct
+	memset(&servaddr, 0, sizeof(servaddr));
+   servaddr.sin_family = AF_INET;		// set to IPv4 for simplicity
+   servaddr.sin_addr.s_addr = htonl(INADDR_ANY); // use my IP
+   servaddr.sin_port = htons(PORT);		// short, network byte order
+	
+	// create the socket file descriptor
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("socket");
+		exit(EXIT_FAILURE);
+	}
+	
+	// bind the socket to the PORT 
+	if (bind(sockfd, (SA*)&servaddr, sizeof(servaddr)) < 0) {
+		perror("bind");
+		exit(EXIT_FAILURE);
+	}
+
+	// since we a re not connecting to a remote host
+	// we will listen on our own system
+	if (listen(sockfd, BACKLOG) < 0) {
+		perror("listen");
+		exit(EXIT_FAILURE);
+	}
+	
+	printf("server waiting for connection...\n");
+
+	// we wait and listen for any connection to our server
+	// if we get a bite, we accept() it and try to successfully
+	// connect. if we do, we fork a new process and send the
+	// message through a new file descriptor
+	while (1) {
+		len = sizeof client_addr;
+		newfd = accept(sockfd, (SA*)&client_addr, &len);
+		if (newfd < 0) {
+			perror("accept");
+			continue;	// keep trying
+		}
+		// create a child process and send the message
+		if (!fork()) {
+			close(sockfd);	// child doesn't need the lister
+			if (send(newfd, time, strlen(time), 0) < 0)
+				perror("send");
+			close(newfd);
+			exit(0);	// succesfully exit the server
+		}
+		close(newfd);	// parent doesn't need this file descriptor
+	}
+	close(sockfd);
+	return 0;
+}
